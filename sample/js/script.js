abcd239d441a2327fff0a8deadc4af166c0b57a8
unknown <alxkdr@icloud.com>
2020-04-16 19:09:50 +0300

    Hamburger, adaptive, design

--- sample/js/script.js
+++ sample/js/script.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows:false,
-        dots:true,
+
+$(function(){
+    $('select').styler();
+    $('.header__btn-menu').on('click', function(){
+      $('.menu ul').slideToggle();
     });
-});
+  
+  });
