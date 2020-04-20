6bd293c74bca09e46858ea8cafa5412ed374e990
unknown <alxkdr@icloud.com>
2020-04-20 12:49:51 +0300

    SLIDER

--- sample/js/script.js
+++ sample/js/script.js
@@ -1,8 +1,17 @@
-
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows:false,
+    dots:true,
+    mobileFirst: true
+    // autoplay: true
+    // adaptiveHeight:true
+  });
+});
 $(function(){
     $('select').styler();
     $('.header__btn-menu').on('click', function(){
-      $('.menu ul').slideToggle();
+    $('.menu ul').slideToggle();
     });
   
   });
+ 
